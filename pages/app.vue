0fa15d231fd232f97346fb4ba8ef5eb712924abd
Ekin Ekinci <ekinekinci@Ekin-MacBook-Air.local>
2023-07-28 19:40:30 +0300

    implement Words component

---
+++ pages/app.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p class="">Some vocab!</p>
+    <div class=" ">
+      <div class="">
+        <nuxtLink to="words"> Words</nuxtLink>
+        
+      </div>
+      
+    <nuxtLink to="quizzes"> Quizzes</nuxtLink>
+    <nuxtLink to="blogs"> Blogs</nuxtLink>
+    </div>
+    
+    
+    
+  </div>
+</template>

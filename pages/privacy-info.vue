51f5218d00fc89b212e7eafa7ad6f69bc390593c
Ekin Ekinci <ekinekinci@Ekin-MacBook-Air.local>
2024-01-05 18:34:20 +0300

    Add privacy statement

---
+++ pages/privacy-info.vue
@@ -0,0 +1,23 @@
+<template>
+    <p>Privacy Statement
+Last Updated: 05.01.2024
+Welcome to Band8plus.com!
+At Band8plus.com, we are committed to upholding your privacy and protecting your personal data. This privacy statement is designed to inform you about our practices regarding the collection, use, and disclosure of personal data in compliance with the General Data Protection Regulation (GDPR).
+1. Personal Data Collection and Use
+We prioritize your privacy and have designed our website to reflect this commitment. As part of our privacy-first approach:
+•	No Personal Data Collection: We do not collect any personal data about our visitors. This includes not tracking user movement or behavior on our website.
+•	No Cookies: Our website does not use cookies or similar tracking technologies. Therefore, there is no storage or retrieval of data on your device about your visit to our site.
+2. Data Protection
+Since we do not collect personal data, there are no risks associated with the misuse or unauthorized access to your personal information from our website.
+3. Third-Party Links
+Our website may contain links to third-party websites. These external sites are not operated by us, and we advise you to review the Privacy Policy of these websites as we have no control over, and assume no responsibility for, the content, privacy policies, or practices of any third-party sites or services.
+4. Your GDPR Rights
+Under GDPR, individuals within the European Union have certain rights concerning their personal data. However, as we do not collect personal data, these rights are not applicable in the context of our website. If you have questions or concerns about your rights under GDPR in general, we encourage you to contact the appropriate data protection authority.
+5. Changes to This Privacy Statement
+We may update our Privacy Statement from time to time. We will notify you of any changes by posting the new Privacy Statement on this page. We advise you to review this Privacy Statement periodically for any changes.
+6. Contact Us
+If you have any questions about this Privacy Statement, please contact us at eekinci367@gmail.com.
+
+
+                                                                      </p>                                                                  
+</template>
